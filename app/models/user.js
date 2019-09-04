b1d8cef1551d67e5491bae2bb79d75eb8628f24d
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-09-04 13:26:53 -0300

    User model and migration

---
+++ app/models/user.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'user',
+    {
+      id: { type: DataTypes.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
+      firstName: { type: DataTypes.STRING, allowNull: false },
+      lastName: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, allowNull: false, unique: true },
+      password: { type: DataTypes.STRING, allowNull: false }
+    },
+    {}
+  );
+  return User;
+};
